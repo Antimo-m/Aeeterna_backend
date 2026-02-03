73c0ef68d6e49c52a51f9435cd2b78ec70d83df9
StivenMastrovito <stivenmastrovito6@gmail.com>
2026-02-03 10:08:50 +0100

    feat: query new Arrivals

---
+++ routers/product.js
@@ -0,0 +1,16 @@
+import express from "express"
+import controller from "../controllers/productController.js"
+
+const router = express.Router();
+
+
+
+
+// INDEX BEST SELLER
+router.get("/bestseller", controller.bestSeller);
+
+// INDEX NEW ARRIVALS
+router.get("/newarrivals", controller.newArrivals);
+
+// INDEX PRODUCT
+router.get("/", controller.index);
