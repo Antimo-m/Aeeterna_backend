ff6498be8227acc60d020d618d92b01b09cd1039
StivenMastrovito <stivenmastrovito6@gmail.com>
2026-02-03 09:53:26 +0100

    feat: controller bestSellere

---
+++ controllers/productController.js
@@ -0,0 +1,28 @@
+import connection from "../db/createConnection.js"
+
+function bestSeller(req, res, next) {
+
+    const query = `
+        select products.*, sum(product_invoice.quantity) as quantity, skin_types.name as type_of_skin, categories.name as category_product
+        from products
+        left join product_invoice
+        on products.id = product_invoice.id_product
+        inner join skin_types
+        on skin_types.id = products.id_skin_type
+        inner join categories   
+        on categories.id = products.id_category
+        group by products.id 
+        order by quantity desc
+        limit 10
+        `;
+
+    connection.query(query, (err, results) => {
+        if(err) return next(err);
+
+        res.json(results)
+    });
+}
+
+export default {
+    bestSeller,
+}
