1bcdd38c51bd902330e201edaa49ad871924c18a
Lucia994 <l.summa94@gmail.com>
2026-02-03 09:56:17 +0100

    fix errorHandler.js

---
+++ middleware/errorHandler.js
@@ -0,0 +1,8 @@
+export default function errorHandler (err, req, res, next){
+
+    res.status(500)
+        .json({
+        error: process.env.ENVIRONMENT === "development" ? err : "INTERNAL ERROR",
+        message: "Internal server error",
+    });
+};
