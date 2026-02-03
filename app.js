37704f7d9862f71bf2a204ae23a9407d4d38e3a5
Antimo-M <montellaantimo@live.it>
2026-02-03 10:25:03 +0100

    aggiunta createConnection

--- app.js
+++ app.js
@@ -1,26 +1,19 @@
 import express from "express";
 import cors from "cors";
+import connection from "./db/createConnection.js"; 
 
+const app = express();
+const port = process.env.PORT || 3000;
 
-const app = express()
-const port = process.env.PORT;
-
-app.use(
-    cors({
-        origin: "http://localhost:5173",
-    }),
-);
-
- 
+app.use(cors(
+    { origin: process.env.FRONTEND_URL }));
 
 app.get("/", (req, res) => {
-    res.send("Aeeterna attivo")
-})
-
+    res.send("Aeeterna attivo");
+});
 
 app.listen(port, () => {
-    console.log(`Server running on ${port}`)
-})
-
+    console.log(`Server running on ${port}`);
+});
 
 
--- db/createConnection.js
+++ db/createConnection.js
@@ -1,23 +1,23 @@
-import mysql2 from "mysql2"
+import mysql2 from "mysql2";
+import dotenv from "dotenv";
 
-import mysql2 from "mysql2"
+dotenv.config();
+
+console.log("DB_HOST:", process.env.DB_HOST); 
 
 const connection = mysql2.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
-})
-
+});
 
 connection.connect((err) => {
     if (err) {
-        console.log(err)
+        console.log(" MySQL connection error:", err.message);
     } else {
-        console.log("connected to mysql")
+        console.log(" Connected to MySQL");
     }
-})
+});
 
-
-
-export default connection
+export default connection;
